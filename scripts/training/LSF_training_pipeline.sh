bf6bccb63ae8d803b7baee784d1a7400d44f1d9d
Mr Dennis Goldfarb <dennisg@killdevil-login1.kd.unc.edu>
2016-12-01 10:34:02 -0500

    changed permissions

--- scripts/training/LSF_training_pipeline.sh
+++ scripts/training/LSF_training_pipeline.sh
@@ -3,10 +3,10 @@
 bsub < LSF_get_sulfur_distribution.sh > out
 JOBID=`head -1 out | sed 's/.*<\\([0-9]*\\)>.*/\\1/'`
 
-bsub < LSF_create_training_data.sh > out
-JOBID=`head -1 out | sed 's/.*<\\([0-9]*\\)>.*/\\1/'`
-rm out
-bsub < LSF_combine_models.sh -w 'numended('$JOBID',>=16)'
+#bsub < LSF_create_training_data.sh > out
+#JOBID=`head -1 out | sed 's/.*<\\([0-9]*\\)>.*/\\1/'`
+#rm out
+#bsub < LSF_combine_models.sh -w 'numended('$JOBID',>=16)'
 
 bsub < LSF_create_average_training_data.sh -w 'ended('LSF_get_sulfur_distribution.sh')' > out
 JOBID=`head -1 out | sed 's/.*<\\([0-9]*\\)>.*/\\1/'`
