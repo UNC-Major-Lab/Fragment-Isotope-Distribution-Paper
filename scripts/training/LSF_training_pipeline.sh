63a4a02b0814078d2d0d3a7393d35b64a6270ef2
DennisGoldfarb <goldfarb.dennis@gmail.com>
2016-12-02 10:22:31 -0500

    split script that combines models

--- scripts/training/LSF_training_pipeline.sh
+++ scripts/training/LSF_training_pipeline.sh
@@ -11,3 +11,4 @@ bsub < LSF_combine_models.sh -w 'numended('$JOBID',>=16)'
 bsub < LSF_create_average_training_data.sh -w 'ended('LSF_get_sulfur_distribution.sh')' > out
 JOBID=`head -1 out | sed 's/.*<\\([0-9]*\\)>.*/\\1/'`
 rm out
+bsub < LSF_combine_averagine_models.sh -w 'ended('$JOBID')
