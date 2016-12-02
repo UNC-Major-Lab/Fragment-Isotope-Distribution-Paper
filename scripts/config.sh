8b911f8cce18d336dae4508e1acc873b0e5dde7e
DennisGoldfarb <goldfarb.dennis@gmail.com>
2016-12-02 14:23:17 -0500

    added fasta file to data folder

--- scripts/config.sh
+++ scripts/config.sh
@@ -1,14 +1,14 @@
 #!/bin/csh
 set SOURCE_DIR="~/Fragment-Isotope-Distribution-Paper/"
-set BUILD_DIR=${SOURCE_DIR}"build/"
+set BUILD_DIR=${SOURCE_DIR}"/build/"
 
 set ROOT_OUT_DIR="/netscr/dennisg/MSFragmentDeisotoper/"
-set SULFUR_OUT_DIR=${ROOT_OUT_DIR}"SulfurCorrected/"
-set AVERAGINE_OUT_DIR=${ROOT_OUT_DIR}"Averagine/"
+set SULFUR_OUT_DIR=${ROOT_OUT_DIR}"/SulfurCorrected/"
+set AVERAGINE_OUT_DIR=${ROOT_OUT_DIR}"/Averagine/"
 
 set LOG_DIR="/netscr/dennisg/log/"
 
-set FASTA="~/fasta/human_sp_112816.fasta"
+set FASTA=${SOURCE_DIR}"/human_sp_112816.fasta"
 
 set NUM_SAMPLES="100"
 set MAX_SAMPLED_MASS="8500"
