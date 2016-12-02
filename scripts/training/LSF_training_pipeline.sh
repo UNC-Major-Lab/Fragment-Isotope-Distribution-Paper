4b4aac80249f2a454667c225978f9d2529e53966
DennisGoldfarb <goldfarb.dennis@gmail.com>
2016-12-02 11:43:01 -0500

    had to split up combining models from creating the models. I can't think of a way for the jobs to wait for each other

--- scripts/training/LSF_training_pipeline.sh
+++ scripts/training/LSF_training_pipeline.sh
@@ -1,14 +1,9 @@
 #!/bin/sh
 
-bsub < LSF_get_sulfur_distribution.sh > out
-JOBID=`head -1 out | sed 's/.*<\\([0-9]*\\)>.*/\\1/'`
-
-bsub < LSF_create_training_data.sh > out
-JOBID=`head -1 out | sed 's/.*<\\([0-9]*\\)>.*/\\1/'`
-rm out
-bsub < LSF_combine_models.sh -w 'numended('$JOBID',>=16)'
-
-bsub < LSF_create_average_training_data.sh -w 'ended('LSF_get_sulfur_distribution.sh')' > out
-JOBID=`head -1 out | sed 's/.*<\\([0-9]*\\)>.*/\\1/'`
-rm out
-bsub < LSF_combine_averagine_models.sh -w 'ended('$JOBID')'
+if ($1 == 1) then
+    bsub < LSF_get_sulfur_distribution.sh
+    bsub < LSF_create_average_training_data.sh -w 'ended('LSF_get_sulfur_distribution.sh')'
+    bsub < LSF_create_training_data.sh
+else if ($1 == 2) then
+    bsub < LSF_combine_models.sh
+endif
