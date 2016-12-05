7a5b24329b8ba615adfc211bc3933c555ef71209
DennisGoldfarb <goldfarb.dennis@gmail.com>
2016-12-05 09:58:35 -0500

    fixed FASTA path

--- scripts/config.sh
+++ scripts/config.sh
@@ -1,6 +1,7 @@
 #!/bin/csh
 set SOURCE_DIR="~/Fragment-Isotope-Distribution-Paper/"
 set BUILD_DIR=${SOURCE_DIR}"/build/"
+set DATA_IDR=${SOURCE_DIR}"/data/"
 
 set ROOT_OUT_DIR="/netscr/dennisg/MSFragmentDeisotoper/"
 set SULFUR_OUT_DIR=${ROOT_OUT_DIR}"/SulfurCorrected/"
@@ -8,7 +9,7 @@ set AVERAGINE_OUT_DIR=${ROOT_OUT_DIR}"/Averagine/"
 
 set LOG_DIR="/netscr/dennisg/log/"
 
-set FASTA=${SOURCE_DIR}"/human_sp_112816.fasta"
+set FASTA=${DATA_DIR}"/human_sp_112816.fasta"
 
 set NUM_SAMPLES="100"
 set MAX_SAMPLED_MASS="8500"
