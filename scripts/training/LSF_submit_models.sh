c06874cc177ea21788ab04d1bccac68dd3896ad4
DennisGoldfarb <goldfarb.dennis@gmail.com>
2016-12-06 21:13:18 -0500

    fixed syntax error

--- scripts/training/LSF_submit_models.sh
+++ scripts/training/LSF_submit_models.sh
@@ -14,7 +14,7 @@ foreach file (${OUT_DIR}/data/*)
     if (-d $file) then
         echo "Skipping $file (is a directory)"
     else
-	    bsub -q day -J "single.sh" -o ${LOG_DIR}"/single.log.%J" -M 20 ${SOURCE_DIR}"scripts/training/single_model.sh" $file $S $CS $Se $CSe $max_mass $OUT_DIR"
+	    bsub -q day -J "single.sh" -o ${LOG_DIR}"/single.log.%J" -M 20 ${SOURCE_DIR}"scripts/training/single_model.sh $file $S $CS $Se $CSe $max_mass $OUT_DIR"
     endif
 end
 
