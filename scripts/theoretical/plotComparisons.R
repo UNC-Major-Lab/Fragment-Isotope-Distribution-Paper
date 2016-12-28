e8fc389dda0d1e50bf1570c431e2957ffb07cced
DennisGoldfarb <goldfarb.dennis@gmail.com>
2016-12-27 21:36:09 -0500

    increased width

--- scripts/theoretical/plotComparisons.R
+++ scripts/theoretical/plotComparisons.R
@@ -10,7 +10,7 @@ data <- read.table(infile,header=F,sep="\t")
 
 p <- ggplot(data=data, aes(x=V2, y=V4, group=V3, fill=factor(V3)))
 
-pdf(outfile, width=6, height=6)
+pdf(outfile, width=9, height=6)
 
 p + geom_bar(stat='identity', width=.01) + facet_wrap(~ V3, scale='free_y') + xlab("residual") + ylab("frequency") + ggtitle("Histogram of residuals (averagine - exact)")
 
