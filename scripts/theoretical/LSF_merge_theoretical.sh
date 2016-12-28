fda3d5deea8c9035df3e0cf447dc4c71ff9fb4d6
DennisGoldfarb <goldfarb.dennis@gmail.com>
2016-12-27 20:36:40 -0500

    bug fix

--- scripts/theoretical/LSF_merge_theoretical.sh
+++ scripts/theoretical/LSF_merge_theoretical.sh
@@ -14,6 +14,6 @@ source ../config.sh
 set BIN_SIZE = 0.01
 set IN_DIR = ${ROOT_OUT_DIR}"/compare_to_theoretical/"
 
-python ${SOURCE_DIR}/scripts/theoretical/mergeTheoreticalComparisons.py $IN_DIR $BIN_SIZE "scores_" $LSB_JOBINDEX 10 > ${IN_DIR}"/merged_scores_"${LSB_JOBINDEX}".txt"
-python ${SOURCE_DIR}/scripts/theoretical/mergeTheoreticalComparisons.py $IN_DIR $BIN_SIZE "residuals_" $LSB_JOBINDEX 10 > ${IN_DIR}"/merged_residuals_"${LSB_JOBINDEX}".txt"
+python ${SOURCE_DIR}/scripts/theoretical/mergeTheoreticalComparisons.py $IN_DIR $BIN_SIZE "scores_" $LSB_JOBINDEX 10 > ${IN_DIR}"/merged_scores_"${LSB_JOBINDEX}".out"
+python ${SOURCE_DIR}/scripts/theoretical/mergeTheoreticalComparisons.py $IN_DIR $BIN_SIZE "residuals_" $LSB_JOBINDEX 10 > ${IN_DIR}"/merged_residuals_"${LSB_JOBINDEX}".out"
 
