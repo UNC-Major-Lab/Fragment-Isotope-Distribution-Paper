3b2c953ccce8713381c8867aca41080c1aff92a9
DennisGoldfarb <goldfarb.dennis@gmail.com>
2017-01-03 22:21:06 -0500

    bug fix

--- scripts/training/single_model.sh
+++ scripts/training/single_model.sh
@@ -11,7 +11,7 @@ set precursor = `echo "$file" | sed -n "s/.*\/[a-zA-Z]*\([0-9]*\).*/\1/p"`
 
 set out_res_hist = ${OUT_DIR}/spline/hist/Precursor${precursor}.pdf
 set out_scatter = ${OUT_DIR}/spline/scatter/Precursor${precursor}.pdf
-set out_res = ${OUT_DIR}/spline/res3D/Precursor${precursor}.pdf
+set out_res = ${OUT_DIR}/spline/res/Precursor${precursor}.pdf
 set out_gof = ${OUT_DIR}/spline/gof/Precursor${precursor}.txt
 set out_models = ${OUT_DIR}/spline/model/Precursor${precursor}.xml
 
