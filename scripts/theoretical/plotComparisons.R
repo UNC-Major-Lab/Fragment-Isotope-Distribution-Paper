c9c7e1b7ed46fcbd446b96e8269d438549b187d0
DennisGoldfarb <goldfarb.dennis@gmail.com>
2017-02-09 11:27:34 -0500

    comparing averagine and sulfur-specific averagine for precursors

--- scripts/theoretical/plotComparisons.R
+++ scripts/theoretical/plotComparisons.R
@@ -5,6 +5,7 @@ args = commandArgs(trailingOnly=TRUE)
 
 infile = args[1]
 outfile = args[2]
+binWidth = as.numeric(args[3])
 
 data <- read.table(infile,header=F,sep="\t")
 
@@ -12,6 +13,6 @@ p <- ggplot(data=data, aes(x=V2, y=V3, group=V1, fill=factor(V1)))
 
 pdf(outfile, width=9, height=6)
 
-p + geom_bar(stat='identity', width=.0025) + facet_wrap(~ V1) + xlab("residual") + ylab("frequency") + ggtitle("Histogram of residuals (averagine - expected)")
+p + geom_bar(stat='identity', width=binWidth) + facet_wrap(~ V1) + xlab("residual") + ylab("frequency") + ggtitle("Histogram of residuals (averagine - expected)")
 
 dev.off()
