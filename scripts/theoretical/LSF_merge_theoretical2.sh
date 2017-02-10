dedb7e5adf2cf379ffbb0344a21839e7693caabe
DennisGoldfarb <goldfarb.dennis@gmail.com>
2017-02-10 13:25:36 -0500

    adding fragment comparisons

--- scripts/theoretical/LSF_merge_theoretical2.sh
+++ scripts/theoretical/LSF_merge_theoretical2.sh
@@ -18,8 +18,8 @@ set IN_DIR = ${ROOT_OUT_DIR}"/compare_to_theoretical/"
 python ${SOURCE_DIR}/scripts/theoretical/mergeHistogram.py $IN_DIR $BIN_SIZE_CHISQUARE "merged_precursor_scores_" 1 10 > ${IN_DIR}"/precursor_scores.txt"
 python ${SOURCE_DIR}/scripts/theoretical/mergeHistogram.py $IN_DIR $BIN_SIZE_RESIDUAL "merged_precursor_residuals_" 1 10 > ${IN_DIR}"/precursor_residuals.txt"
 
-python ${SOURCE_DIR}/scripts/theoretical/mergeHistogram.py $IN_DIR $BIN_SIZE_CHISQUARE "merged_fragment_scores_" 1 10 > ${IN_DIR}"/fragment_scores.txt"
-python ${SOURCE_DIR}/scripts/theoretical/mergeHistogram.py $IN_DIR $BIN_SIZE_RESIDUAL "merged_fragment_residuals_" 1 10 > ${IN_DIR}"/fragment_residuals.txt"
+#python ${SOURCE_DIR}/scripts/theoretical/mergeHistogram.py $IN_DIR $BIN_SIZE_CHISQUARE "merged_fragment_scores_" 1 10 > ${IN_DIR}"/fragment_scores.txt"
+#python ${SOURCE_DIR}/scripts/theoretical/mergeHistogram.py $IN_DIR $BIN_SIZE_RESIDUAL "merged_fragment_residuals_" 1 10 > ${IN_DIR}"/fragment_residuals.txt"
 
 Rscript plotComparisons.R ${IN_DIR}"/precursor_residuals.txt" ${IN_DIR}"/precursor_residuals.pdf" ${BIN_SIZE_RESIDUAL}
 Rscript plotComparisons.R ${IN_DIR}"/precursor_scores.txt" ${IN_DIR}"/precursor_chisquared.pdf" ${BIN_SIZE_CHISQUARE}
