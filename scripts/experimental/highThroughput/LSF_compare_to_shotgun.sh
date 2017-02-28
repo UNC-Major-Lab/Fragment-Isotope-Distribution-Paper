60cf0b5f647b92a5e3a44b53b84788e1c1958f29
DennisGoldfarb <goldfarb.dennis@gmail.com>
2017-02-28 12:31:14 -0500

    fixing scripts

--- scripts/experimental/highThroughput/LSF_compare_to_shotgun.sh
+++ scripts/experimental/highThroughput/LSF_compare_to_shotgun.sh
@@ -4,7 +4,7 @@
 #BSUB -q week
 #BSUB -o /netscr/dennisg/log/LSF_compare_to_shotgun.log.%J
 #BSUB -n 1
-#BSUB -M 1
+#BSUB -M 4
 
 module load gcc/4.8.1
 module load r/3.2.2
@@ -16,4 +16,4 @@ set OUT_DIR = ${ROOT_OUT_DIR}"/compare_to_experimental/HT"
 mkdir -p $OUT_DIR
 
 ${BUILD_DIR}/CompareToShotgun ${DATA_DIR}"/HELA_2017-01-31_5.mzML" ${DATA_DIR}"/HELA_2017-01-31_5.idXML" 0.0 $OUT_DIR
-Rscript PlotShotgunResults.R ${OUT_DIR}"/distributionScores.out" $OUT_DIR
+Rscript highThroughput/PlotShotgunResults.R ${OUT_DIR}"/distributionScores.out" $OUT_DIR
