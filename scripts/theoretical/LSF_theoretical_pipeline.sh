2550173da6f750dee90b7c066b5f61c500544c1b
DennisGoldfarb <goldfarb.dennis@gmail.com>
2017-03-08 14:17:49 -0500

    increasing number of jobs

--- scripts/theoretical/LSF_theoretical_pipeline.sh
+++ scripts/theoretical/LSF_theoretical_pipeline.sh
@@ -8,4 +8,4 @@ bsub < LSF_merge_theoretical.sh -w 'numended('$JOBID',>=100)' > out
 JOBID=`head -1 out | sed 's/.*<\\([0-9]*\\)>.*/\\1/'`
 rm out
 
-bsub < LSF_merge_theoretical2.sh -w 'numended('$JOBID',>=10)'
+bsub < LSF_merge_theoretical2.sh -w 'numended('$JOBID',>=50)'
