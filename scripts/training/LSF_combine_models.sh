eb82fff4a03084fd8242b79ccf2ec19df4b37930
DennisGoldfarb <goldfarb.dennis@gmail.com>
2017-03-16 15:05:46 -0400

    merging all isotope splines into a single file

--- scripts/training/LSF_combine_models.sh
+++ scripts/training/LSF_combine_models.sh
@@ -9,5 +9,4 @@ module load python/2.7.1
 
 source ../config.sh
 
-python ${SOURCE_DIR}/scripts/training/combineModels.py $SULFUR_OUT_DIR > ${SULFUR_OUT_DIR}"/SulfurIsotopeSplines.xml"
-python ${SOURCE_DIR}/scripts/training/combineModels.py $AVERAGINE_OUT_DIR > ${AVERAGINE_OUT_DIR}"/IsotopeSplines.xml"
+python ${SOURCE_DIR}/scripts/training/combineModels.py $AVERAGINE_OUT_DIR $SULFUR_OUT_DIR $MAX_ISOTOPE $MAX_SULFUR > ${AVERAGINE_OUT_DIR}"/IsotopeSplines.xml"
--- scripts/training/combineModels.py
+++ scripts/training/combineModels.py
@@ -3,18 +3,25 @@ import sys
 import os
 import re
 
+def iterateAndPrint(root_dir):
+    for d in os.listdir(root_dir):
+        if not os.path.isfile(d) and os.path.exists(root_dir+d+"/spline/model/"):
+            for f in os.listdir(root_dir+d+"/spline/model/"):
+                fp = root_dir+d+"/spline/model/"+f
+                if os.path.isfile(fp):
+                    infile = open(fp)
+                    for line in infile:
+                        print line.rstrip()
+
 root_dir = sys.argv[1]
+root_sulfur_dir = sys.argv[2]
+max_isotope = sys.argv[3]
+max_sulfur = sys.argv[4]
 
 print "<?xml version=\"1.0\" encoding=\"UTF-8\"?>"
-print "<models>\n"
+print "<models max_isotope=\"" + max_isotope + "\" max_sulfur=\"" + max_sulfur + "\">\n"
 
-for d in os.listdir(root_dir):
-    if not os.path.isfile(d) and os.path.exists(root_dir+d+"/spline/model/"):
-        for f in os.listdir(root_dir+d+"/spline/model/"):
-            fp = root_dir+d+"/spline/model/"+f
-            if os.path.isfile(fp):
-                infile = open(fp)
-                for line in infile:
-                    print line.rstrip()
+iterateAndPrint(root_dir);
+iterateAndPrint(root_sulfur_dir);
 
 print "</models>"
