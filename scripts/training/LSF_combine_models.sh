37a717cc209708577e914320ef2eb31a0efdf0da
DennisGoldfarb <goldfarb.dennis@gmail.com>
2017-03-23 15:34:23 -0400

    cleaning up code

--- scripts/training/LSF_combine_models.sh
+++ scripts/training/LSF_combine_models.sh
@@ -10,4 +10,4 @@ module load python/2.7.1
 
 source ../config.sh
 
-python ${SOURCE_DIR}/scripts/training/combineModels.py $SPLINE_OUT_DIR $MAX_ISOTOPE $MAX_SULFUR > ${SPLINE_OUT_DIR}"/IsotopeSplines.xml"
+python ${SOURCE_DIR}/scripts/training/combineModels.py $SPLINE_OUT_DIR $MAX_DEPTH $MAX_SULFUR > ${SPLINE_OUT_DIR}"/IsotopeSplines.xml"
