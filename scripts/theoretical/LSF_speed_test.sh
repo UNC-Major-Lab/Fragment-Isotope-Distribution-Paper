318865ace53d85de395a5121c340193ce63ac493
DennisGoldfarb <goldfarb.dennis@gmail.com>
2017-03-23 14:38:01 -0400

    cleaning up code

---
+++ scripts/theoretical/LSF_speed_test.sh
@@ -0,0 +1,19 @@
+#!/bin/csh
+#BSUB -L /bin/csh
+#BSUB -J LSF_speed_test.sh
+#BSUB -q week
+#BSUB -o /netscr/dennisg/log/LSF_speed_test.log.%J
+#BSUB -n 1
+#BSUB -M 1
+
+source ../config.sh
+
+set IN_DIR = ${ROOT_OUT_DIR}"/compare_to_theoretical/"
+
+set MIN_MASS = 400
+set MAX_MASS = 9500
+set MAX_DEPTH = 11
+set NUM_TESTS = 1e5
+
+${BUILD_DIR}/SpeedTest $MIN_MASS $MAX_MASS $MAX_DEPTH $NUM_TESTS > ${OUT_DIR}"/runtimes.out"
+Rscript plotRuntimeComparisons.R ${OUT_DIR}"/runtimes.out" {OUT_DIR}"/runtimes.eps"
--- scripts/theoretical/LSF_theoretical_pipeline.sh
+++ scripts/theoretical/LSF_theoretical_pipeline.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
 
+bsub < LSF_speed_test.sh
+
 bsub < LSF_compare_to_theoretical.sh > out
 JOBID=`head -1 out | sed 's/.*<\\([0-9]*\\)>.*/\\1/'`
 rm out
