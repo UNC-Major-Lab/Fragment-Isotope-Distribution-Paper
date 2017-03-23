5c3ea34f7c2526c4e98432b8adc05339e2068353
DennisGoldfarb <goldfarb.dennis@gmail.com>
2017-03-23 14:21:07 -0400

    cleaning up code

--- scripts/training/LSF_training_pipeline.sh
+++ scripts/training/LSF_training_pipeline.sh
@@ -10,6 +10,7 @@ JOBID=`head -1 out | sed 's/.*<\\([0-9]*\\)>.*/\\1/'`
 rm out
 
 MAX_TRAINING_JOBS=`expr $MAX_SULFUR + 2`
+echo $MAX_SULFUR
 echo $MAX_TRAINING_JOBS
 
 bsub < LSF_combine_models.sh  -w 'numended('$JOBID',>='$MAX_TRAINING_JOBS')'
