02980cc513b9600e303b0e5c6dc7d9ae39897205
DennisGoldfarb <goldfarb.dennis@gmail.com>
2017-04-03 13:23:21 -0400

    added parameter for spline break size

--- scripts/training/single_model.sh
+++ scripts/training/single_model.sh
@@ -15,7 +15,7 @@ set out_res = ${OUT_DIR}/spline/res/Precursor${precursor}.pdf
 set out_gof = ${OUT_DIR}/spline/gof/Precursor${precursor}.txt
 set out_models = ${OUT_DIR}/spline/model/Precursor${precursor}.xml
 
-set param = "IsotopeSpline('"${SPLINE_BREAKS_SIZE}"','"${S}"','"${precursor}"','"${file}"','"${out_res_hist}"','"${out_scatter}"','"${out_res}"','"${out_gof}"','"${out_models}"');quit force"
+set param = "IsotopeSpline("${SPLINE_BREAKS_SIZE}",'"${S}"','"${precursor}"','"${file}"','"${out_res_hist}"','"${out_scatter}"','"${out_res}"','"${out_gof}"','"${out_models}"');quit force"
 
 cd ${SOURCE_DIR}/scripts/training/
 matlab -nodisplay -nosplash -nodesktop -r $param
