4c019361f557b6624f1ae70f319f1169ea4fb54d
DennisGoldfarb <goldfarb.dennis@gmail.com>
2017-04-25 13:55:10 -0400

    adding plots for models

--- scripts/training/LSF_plots.sh
+++ scripts/training/LSF_plots.sh
@@ -13,4 +13,6 @@ set dir = "S0"
 set DATA_DIR = ${SPLINE_OUT_DIR}"/"
 set OUT_DIR = ${ROOT_OUT_DIR}"/training/"
 
+mkdir $OUT_DIR
+
 Rscript plotModel.R ${DATA_DIR}"spline_eval/" ${DATA_DIR} 0 $MAX_SULFUR ${OUT_DIR}"Precursor0_model.eps"
