f606b249fe440642302708e28fb3bae480d6e676
DennisGoldfarb <goldfarb.dennis@gmail.com>
2017-04-26 16:42:23 -0400

    plots

--- scripts/training/plotModel.R
+++ scripts/training/plotModel.R
@@ -48,7 +48,7 @@ p <- ggplot(data, aes(x=precursor.mass, y=probability, color=as.factor(S)))
 print(
   p
   + geom_point(shape=1)
-  + geom_line(data=data.spline, aes(x=precursor.mass, y=probability))
+  + geom_line(data=data.spline, aes(x=precursor.mass, y=probability), color="black")
   )
 
 dev.off()
