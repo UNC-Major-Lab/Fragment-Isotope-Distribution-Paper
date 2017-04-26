cee30157f3b3bc290b31850e17f8cf372c770ecb
DennisGoldfarb <goldfarb.dennis@gmail.com>
2017-04-26 16:52:54 -0400

    plots

--- scripts/training/plotModel.R
+++ scripts/training/plotModel.R
@@ -22,7 +22,7 @@ if (max.sulfur == -1) {
   spline.infile <- paste(data.basedir, "Average_Spline", "/spline/eval/Precursor", precursor, ".tab", sep="")
   data.spline <- read.table(spline.infile, header=T, sep="\t")
   data.spline <- subset(data.spline, data.spline$precursor.mass <= max.mass)
-  data.spline$C <- -1
+  data.spline$S <- -1
 } else {
   for (sulfur in 0:max.sulfur) {
     scatter.infile <- paste(data.basedir, "S", toString(sulfur), "/data/Precursor", precursor, ".tab", sep="")
@@ -33,8 +33,8 @@ if (max.sulfur == -1) {
     
     spline.infile <- paste(data.basedir, "S", toString(sulfur), "/spline/eval/Precursor", precursor, ".tab", sep="")
     data.spline.tmp <- read.table(spline.infile, header=T, sep="\t")
-    data.spline.tmp <- subset(data.spline/tmp, data.spline.tmp$precursor.mass <= max.mass)
-    data.spline.tmp$C <- sulfur
+    data.spline.tmp <- subset(data.spline.tmp, data.spline.tmp$precursor.mass <= max.mass)
+    data.spline.tmp$S <- sulfur
     data.spline <- rbind(data.spline, data.spline.tmp)
   }
 }
@@ -48,7 +48,7 @@ p <- ggplot(data, aes(x=precursor.mass, y=probability, color=as.factor(S)))
 print(
   p
   + geom_point(shape=1)
-  + geom_line(data=data.spline, aes(x=precursor.mass, y=probability, group=C), color="black")
+  + geom_line(data=data.spline, aes(x=precursor.mass, y=probability, group=S), color="black")
   )
 
 dev.off()
