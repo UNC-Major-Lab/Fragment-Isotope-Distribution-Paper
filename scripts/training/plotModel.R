f765330710374a58fc469196e43b3ff29b7a9a86
DennisGoldfarb <goldfarb.dennis@gmail.com>
2017-04-27 14:26:00 -0400

    plots

--- scripts/training/plotModel.R
+++ scripts/training/plotModel.R
@@ -12,20 +12,19 @@ max.mass <- as.numeric(args[6])
 
 data <- data.frame()
 data.spline <- data.frame()
-data.spline.all <- data.frame()
 
-spline.infile <- paste(data.basedir, "Average_Spline", "/spline/eval/Precursor", precursor, ".tab", sep="")
-data.spline.all <- read.table(spline.infile, header=T, sep="\t")
-data.spline.all <- subset(data.spline, data.spline$precursor.mass <= max.mass)
-data.spline.all$S <- "All"
+
 
 if (max.sulfur == -1) {
   scatter.infile <- paste(data.basedir, "Average_Spline", "/data/Precursor", precursor, ".tab", sep="")
   data <- read.table(scatter.infile, header=T, sep="\t")
   data <- subset(data, data$precursor.mass <= max.mass)
   data$S <- "All"
-  data.spline.all$line.type <- "solid"
-  data.spline.all$color <- "black"
+  
+  spline.infile <- paste(data.basedir, "Average_Spline", "/spline/eval/Precursor", precursor, ".tab", sep="")
+  data.spline <- read.table(spline.infile, header=T, sep="\t")
+  data.spline <- subset(data.spline, data.spline$precursor.mass <= max.mass)
+  data.spline$S <- "All"
 } else {
   for (sulfur in 0:max.sulfur) {
     scatter.infile <- paste(data.basedir, "S", toString(sulfur), "/data/Precursor", precursor, ".tab", sep="")
@@ -38,11 +37,8 @@ if (max.sulfur == -1) {
     data.spline.tmp <- read.table(spline.infile, header=T, sep="\t")
     data.spline.tmp <- subset(data.spline.tmp, data.spline.tmp$precursor.mass <= max.mass)
     data.spline.tmp$S <- toString(sulfur)
-    data.spline.tmp$line.type <- 'solid'
     data.spline <- rbind(data.spline, data.spline.tmp)
   }
-  data.spline.all$line.type <- "dashed"
-  data.spline.all$color <- 'red'
 }
   
   
@@ -55,9 +51,6 @@ print(
   p
   + geom_point(shape=1)
   + geom_line(data=data.spline, aes(x=precursor.mass, y=probability, group=S, linetype=line.type), color="black")
-  + geom_line(data=data.spline.all, aes(x=precursor.mass, y=probability, linetype=line.type, color=color))
-  + scale_linetype_identity()
-  + scale_color_identity()
   )
 
 dev.off()
