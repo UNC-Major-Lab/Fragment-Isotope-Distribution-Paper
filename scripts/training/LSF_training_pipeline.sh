8e0456fe829bc2ef1bc7ea21060850e4102888ab
DennisGoldfarb <goldfarb.dennis@gmail.com>
2017-04-28 12:34:05 -0400

    plots

--- scripts/training/LSF_training_pipeline.sh
+++ scripts/training/LSF_training_pipeline.sh
@@ -5,8 +5,8 @@ source ../config.sh
 if ($1 == 1) then
     bsub < LSF_get_sulfur_distribution.sh
     bsub < LSF_create_training_data.sh
+    bsub < LSF_create_eval_data.sh
 else if ($1 == 2) then
     bsub < LSF_combine_models.sh
-    bsub < LSF_create_eval_data.sh
-    bsub < LSF_plots.sh -w 'done("LSF_create_eval_data.sh")'
+    bsub < LSF_plots.sh
 endif
