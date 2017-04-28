7d8b2b3f51e08c9008275b7bb63752cb31c0b75c
DennisGoldfarb <goldfarb.dennis@gmail.com>
2017-04-28 15:13:42 -0400

    plots

---
+++ scripts/theoretical/LSF_plots.sh
@@ -0,0 +1,17 @@
+#!/bin/csh
+#BSUB -L /bin/csh
+#BSUB -J LSF_plots.sh[1-21]
+#BSUB -q hour
+#BSUB -o /netscr/dennisg/log/theoretical_plots.log.%J
+#BSUB -n 1
+
+module load r/3.2.2
+
+source ../config.sh
+
+set DATA_DIR = ${SPLINE_OUT_DIR}"/"
+set OUT_DIR = ${ROOT_OUT_DIR}"/compare_to_theoretical/"
+
+set PRECURSOR_ISOTOPE = `expr $LSB_JOBINDEX - 1`
+
+Rscript plotModel.R ${DATA_DIR}"spline_eval/" ${ROOT_OUT_DIR} $PRECURSOR_ISOTOPE $MAX_SULFUR ${OUT_DIR}"spline_comparison"${PRECURSOR_ISOTOPE}"_model.eps" 10000
---
+++ scripts/theoretical/plotModelToProteome.R
@@ -0,0 +1,56 @@
+#!/usr/bin/env Rscript
+library(ggplot2)
+
+args = commandArgs(trailingOnly=TRUE)
+
+spline.infile <- args[1]
+data.basedir <- args[2]
+precursor <- args[3]
+max.sulfur <- args[4]
+outfile <- args[5]
+max.mass <- as.numeric(args[6])
+
+
+# Proteome theoretical data
+scatter.infile <- paste(data.basedir, "training/proteome/Precursor", precursor, ".tab", sep="")
+data <- read.table(scatter.infile, header=T, sep="\t")
+data <- subset(data, data$precursor.mass <= max.mass)
+
+# Proteome averagine eval
+averagine.infile <- paste(data.basedir, "training/proteome/averagine/Precursor", precursor, ".tab", sep="")
+data.averagine <- read.table(averagine.infile, header=T, sep="\t")
+data.averagine <- subset(data.averagine, data.averagine$precursor.mass <= max.mass)
+
+# Average spline
+spline.infile <- paste(data.basedir, "Averagine/Average_Spline", "/spline/eval/Precursor", precursor, ".tab", sep="")
+data.spline.avg <- read.table(spline.infile, header=T, sep="\t")
+data.spline.avg <- subset(data.spline.avg, data.spline$precursor.mass <= max.mass)
+
+# Sulfur-specific splines
+data.spline <- data.frame()
+for (sulfur in 0:max.sulfur) {
+  spline.infile <- paste(data.basedir, "Averagine/S", toString(sulfur), "/spline/eval/Precursor", precursor, ".tab", sep="")
+  data.spline.tmp <- read.table(spline.infile, header=T, sep="\t")
+  data.spline.tmp <- subset(data.spline.tmp, data.spline.tmp$precursor.mass <= max.mass)
+  data.spline.tmp$S <- toString(sulfur)
+  data.spline <- rbind(data.spline, data.spline.tmp)
+}
+  
+  
+setEPS()
+postscript(outfile, width=9, height=6)
+
+p <- ggplot(data, aes(x=precursor.mass, y=probability, color=S))
+
+print(
+  p
+  + geom_point(shape=1)
+  # sulfur-specific splines
+  + geom_line(data=data.spline, aes(x=precursor.mass, y=probability, group=S), color="black")
+  # average spline
+  + geom_line(data=data.spline.avg, aes(x=precursor.mass, y=probability), color="red", linetype="dashed")
+  # averagine model
+  + geom_line(data=data.averagine, aes(x=precursor.mass, y=probability), color="blue", linetype="dashed")
+  )
+
+dev.off()
