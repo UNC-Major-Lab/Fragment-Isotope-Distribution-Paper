5394c2dbd7d280380930c9f9ff51461b379e2056
DennisGoldfarb <goldfarb.dennis@gmail.com>
2017-05-10 17:22:12 -0400

    plots

--- scripts/experimental/highThroughput/LSF_compare_to_shotgun.sh
+++ scripts/experimental/highThroughput/LSF_compare_to_shotgun.sh
@@ -17,17 +17,27 @@ mkdir -p $OUT_DIR
 ${BUILD_DIR}/CompareToShotgun ${DATA_DIR}"/HELA_2017-01-31_5.mzML" ${DATA_DIR}"/HELA_2017-01-31_5.idXML" 0.0 $OUT_DIR
 Rscript PlotShotgunResults.R ${OUT_DIR}"/distributionScores.out" $OUT_DIR
 
+
+set OUT_DIR = ${ROOT_OUT_DIR}"/compare_to_experimental/HT_HELA_SIM"
+mkdir -p $OUT_DIR
+
+${BUILD_DIR}/CompareToShotgun ${DATA_DIR}"/HELA_2017-05-10_117_SIM_alternating.mzML" ${DATA_DIR}"/HELA_2017-05-10_117_SIM_alternating.idXML" 0.0 $OUT_DIR
+Rscript PlotShotgunResults.R ${OUT_DIR}"/distributionScores.out" ${OUT_DIR}
+
+
 set OUT_DIR = ${ROOT_OUT_DIR}"/compare_to_experimental/HT_BSA_DDA"
 mkdir -p $OUT_DIR
 
 ${BUILD_DIR}/CompareToShotgun ${DATA_DIR}"/BSA_2017-04-29_346_DDA.mzML" ${DATA_DIR}"/BSA_2017-04-29_346_DDA.idXML" 0.0 $OUT_DIR
 Rscript PlotShotgunResults.R ${OUT_DIR}"/distributionScores.out" ${OUT_DIR}
 
+
 set OUT_DIR = ${ROOT_OUT_DIR}"/compare_to_experimental/HT_BSA_SIM_30k"
 mkdir -p $OUT_DIR
 
 ${BUILD_DIR}/CompareToShotgun ${DATA_DIR}"/BSA_2017-05-05_59_SIM.mzML" ${DATA_DIR}"BSA_2017-05-05_59_SIM.idXML" 0.0 $OUT_DIR
 Rscript PlotShotgunResults.R ${OUT_DIR}"/distributionScores.out" ${OUT_DIR}
+
 
 set OUT_DIR = ${ROOT_OUT_DIR}"/compare_to_experimental/HT_BSA_SIM_15k"
 mkdir -p $OUT_DIR
