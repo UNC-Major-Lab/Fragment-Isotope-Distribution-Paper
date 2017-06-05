eb799f4286dff48ce03f03a529661ef7871020c8
DennisGoldfarb <goldfarb.dennis@gmail.com>
2017-06-05 13:44:30 -0400

    added isolation calibration plots

--- scripts/experimental/calibration/PlotCalibration.R
+++ scripts/experimental/calibration/PlotCalibration.R
@@ -12,10 +12,10 @@ outPath <- args[2]
 
 data <- read.table(infile1, header=T, sep="\t")
 
-p <- ggplot(data=data, aes(x=offset, y=intensity))
-  + geom_point()
+p <- ggplot(data=data, aes(x=offset, y=intensity)) +
+  geom_point() +
   #+ geom_smooth(method='loess', aes(color=as.factor(target)))
-  + facet_wrap(width ~ target, scale="free")
+  facet_wrap(width ~ target, scale="free")
 
 
 savePlot(p, outPath)
