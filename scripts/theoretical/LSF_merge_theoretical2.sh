6bb164a5570328d5b6cd873d717710b97059b644
DennisGoldfarb <goldfarb.dennis@gmail.com>
2017-08-18 11:39:37 -0400

    shotgun changes

--- scripts/theoretical/LSF_merge_theoretical2.sh
+++ scripts/theoretical/LSF_merge_theoretical2.sh
@@ -15,17 +15,17 @@ set BIN_SIZE_RESIDUAL = 0.0025
 set BIN_SIZE_CHISQUARE = 0.025
 set IN_DIR = ${ROOT_OUT_DIR}"/compare_to_theoretical/"
 
-python ${SOURCE_DIR}/scripts/theoretical/mergeHistogram.py $IN_DIR "scores_precursor_" F > ${IN_DIR}"/precursor_scores.txt"
-python ${SOURCE_DIR}/scripts/theoretical/mergeHistogram.py $IN_DIR "residuals_precursor_" F > ${IN_DIR}"/precursor_residuals.txt"
+#python ${SOURCE_DIR}/scripts/theoretical/mergeHistogram.py $IN_DIR "scores_precursor_" F > ${IN_DIR}"/precursor_scores.txt"
+#python ${SOURCE_DIR}/scripts/theoretical/mergeHistogram.py $IN_DIR "residuals_precursor_" F > ${IN_DIR}"/precursor_residuals.txt"
 
 python ${SOURCE_DIR}/scripts/theoretical/mergeHistogram.py $IN_DIR "scores_fragment_" T > ${IN_DIR}"/fragment_scores.txt"
 python ${SOURCE_DIR}/scripts/theoretical/mergeHistogram.py $IN_DIR "residuals_fragment_" T > ${IN_DIR}"/fragment_residuals.txt"
 
 python ${SOURCE_DIR}/scripts/theoretical/mergeStats.py $IN_DIR "stats_fragment_" T > ${IN_DIR}"/fragment_stats.txt"
-python ${SOURCE_DIR}/scripts/theoretical/mergeStats.py $IN_DIR "stats_precursor_" F > ${IN_DIR}"/precursor_stats.txt"
+#python ${SOURCE_DIR}/scripts/theoretical/mergeStats.py $IN_DIR "stats_precursor_" F > ${IN_DIR}"/precursor_stats.txt"
 
-Rscript plotComparisons.R ${IN_DIR}"/precursor_residuals.txt" ${IN_DIR}"/precursor_residuals.eps" ${BIN_SIZE_RESIDUAL} F
-Rscript plotComparisons.R ${IN_DIR}"/precursor_scores.txt" ${IN_DIR}"/precursor_chisquared.eps" ${BIN_SIZE_CHISQUARE} F
+#Rscript plotComparisons.R ${IN_DIR}"/precursor_residuals.txt" ${IN_DIR}"/precursor_residuals.eps" ${BIN_SIZE_RESIDUAL} F
+#Rscript plotComparisons.R ${IN_DIR}"/precursor_scores.txt" ${IN_DIR}"/precursor_chisquared.eps" ${BIN_SIZE_CHISQUARE} F
 
 Rscript plotComparisons.R ${IN_DIR}"/fragment_residuals.txt" ${IN_DIR}"/fragment_residuals.eps" ${BIN_SIZE_RESIDUAL} T
 Rscript plotComparisons.R ${IN_DIR}"/fragment_scores.txt" ${IN_DIR}"/fragment_chisquared.eps" ${BIN_SIZE_CHISQUARE} T
