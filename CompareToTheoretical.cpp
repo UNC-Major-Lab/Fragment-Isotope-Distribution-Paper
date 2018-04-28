8450df381e9a8568775aea56f3db52a19a50f48e
DennisGoldfarb <goldfarb.dennis@gmail.com>
2018-04-28 17:20:49 -0400

    script to make directories

--- CompareToTheoretical.cpp
+++ CompareToTheoretical.cpp
@@ -494,7 +494,7 @@ void init(bool doFragments)
     if (doFragments)
     {
         for (UInt start = 0; start <= MAX_ISOTOPE; ++start) {
-            for (UInt i = 1; i <= MAX_ISOTOPE; ++i) {
+            for (UInt i = start; i <= MAX_ISOTOPE; ++i) {
                 if (start == 0 && i == 0) continue;
                 std::string label = std::to_string(start) + "-" + std::to_string(i);
                 fragment_method2iso2val["Averagine"][label] = std::make_pair(std::vector<double>(), std::vector<double>());
