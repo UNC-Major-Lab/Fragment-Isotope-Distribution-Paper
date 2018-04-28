12314777d2cfcf7218673a1bf68ac566115125cb
DennisGoldfarb <goldfarb.dennis@gmail.com>
2018-04-28 15:59:08 -0400

    script to make directories

--- scripts/make_dirs.sh
+++ scripts/make_dirs.sh
@@ -1,20 +1,20 @@
 #!/usr/bin/env bash
 
 mkdir -p out/Average_Spline/spline/model/
-mkdir out/Average_Spline/out/spline/gof/
-mkdir out/Average_Spline/out/spline/hist/
-mkdir out/Average_Spline/out/spline/res/
-mkdir out/Average_Spline/out/spline/scatter/
-mkdir out/Average_Spline/out/spline/eval/
+mkdir out/Average_Spline/spline/gof/
+mkdir out/Average_Spline/spline/hist/
+mkdir out/Average_Spline/spline/res/
+mkdir out/Average_Spline/spline/scatter/
+mkdir out/Average_Spline/spline/eval/
 
 for S in $(seq 0 5);
 do
     set dir = "S$S"
     mkdir -p out/${dir}/spline/model/
-    mkdir out/${dir}/out/spline/gof/
-    mkdir out/${dir}/out/spline/hist/
-    mkdir out/${dir}/out/spline/res/
-    mkdir out/${dir}/out/spline/scatter/
-    mkdir out/${dir}/out/spline/eval/
+    mkdir out/${dir}/spline/gof/
+    mkdir out/${dir}/spline/hist/
+    mkdir out/${dir}/spline/res/
+    mkdir out/${dir}/spline/scatter/
+    mkdir out/${dir}/spline/eval/
 done
 
